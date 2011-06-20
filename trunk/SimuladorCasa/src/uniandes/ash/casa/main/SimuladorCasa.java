b02e0edc96409423d637be0cf0b05a49736a7611
juan.camilo.sanchez.rojas <juan.camilo.sanchez.rojas@00d982ce-deb4-3029-8c76-f4000392ff54>
2011-06-20 03:30:47 +0000

    prueba

--- trunk/SimuladorCasa/src/uniandes/ash/casa/main/SimuladorCasa.java
+++ trunk/SimuladorCasa/src/uniandes/ash/casa/main/SimuladorCasa.java
@@ -5,10 +5,6 @@
 package uniandes.ash.casa.main;
 
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.ArrayList;
 import uniandes.ash.casa.hilo.HiloAntena;
 
 /**
@@ -22,16 +18,14 @@ public class SimuladorCasa {
      */
     public static void main(String[] args) throws IOException {
         try {
-            while(true){
-                HiloAntena ha1=new HiloAntena();
-                HiloAntena ha2=new HiloAntena();
-                HiloAntena ha3=new HiloAntena();
-                HiloAntena ha4=new HiloAntena();
-                ha1.start();
-                ha2.start();
-                ha3.start();
-                ha4.start();
-            }
+            HiloAntena ha1=new HiloAntena();
+            HiloAntena ha2=new HiloAntena();
+            HiloAntena ha3=new HiloAntena();
+            HiloAntena ha4=new HiloAntena();
+            ha1.start();
+            ha2.start();
+            ha3.start();
+            ha4.start();
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Could not listen on port: 4444.");
