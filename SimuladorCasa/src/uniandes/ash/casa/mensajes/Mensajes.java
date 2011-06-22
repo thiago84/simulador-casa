263b1fc6b596618ce3f042c949a7701ff5f9efc2
juan.camilo.sanchez.rojas <juan.camilo.sanchez.rojas@00d982ce-deb4-3029-8c76-f4000392ff54>
2011-06-22 16:14:43 +0000

    cambio numero personas en la casa

--- SimuladorCasa/src/uniandes/ash/casa/mensajes/Mensajes.java
+++ SimuladorCasa/src/uniandes/ash/casa/mensajes/Mensajes.java
@@ -16,24 +16,32 @@ public class Mensajes {
     public String generarMensaje(){
         mensaje=new StringBuffer();
         Random r = new Random();
+        //Estado del sensor 0 estado normal, 1 en larma
         int valorDado = r.nextInt(2);
         mensaje.append(valorDado);
+        //Tipo de sensor 0 Sensor de Movimiento, 1 Sensor de incendios
         valorDado = r.nextInt(2);
         mensaje.append(valorDado);
+        //Tipo de Espacio 0 Casa, 1 Oficinas
         valorDado = r.nextInt(2);
         mensaje.append(valorDado);
+        //Ubicacion dentro del Inmueble
         valorDado = r.nextInt(21);
         if(valorDado<10){
             mensaje.append("0").append(valorDado);
         }else{
             mensaje.append(valorDado);
         }
+        //Objeto que emite la trama
         valorDado = r.nextInt(51);
         if(valorDado<10){
             mensaje.append("0").append(valorDado);
         }else{
             mensaje.append(valorDado);
         }
+        //Personas que se encuentran en esa ubicacion
+        valorDado = r.nextInt(3);
+        mensaje.append(valorDado);
         return mensaje.toString();
     }
 
